fcddd9366eca8272c81e0d5e1f2ea72e2ad0477a
Christian Neff <christian.neff@gmail.com>
2017-10-31 17:24:24 +0100

    Update file docblocks

--- tests/CompilerTest.php
+++ tests/CompilerTest.php
@@ -1,24 +1,16 @@
 <?php
 /**
- * Compiler Library
- * Copyright (C) 2015 secondtruth
+ * PHAR Compiler Library
+ * Copyright (C) 2017 Christian Neff
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
  * above copyright notice and this permission notice appear in all copies.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
- * FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
- * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
- * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
- * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- *
  * @package  Secondtruth\Compiler
  * @version  1.1
  * @link     http://www.secpndtruth.de
- * @license  ISC License <http://opensource.org/licenses/ISC>
+ * @license  http://opensource.org/licenses/ISC ISC License
  */
 
 namespace Secondtruth\Compiler\Tests;
